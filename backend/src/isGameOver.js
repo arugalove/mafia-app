033ab6986d98a3a8271e0b49f04ec7fcff1cc166
Steven Yi <steven.yi@berkeley.edu>
2021-04-28 00:07:49 -0600

    isGameOver works

--- backend/src/isGameOver.js
+++ backend/src/isGameOver.js
@@ -1,21 +1,17 @@
 export const isGameOver = (gameState) => {
+
+    const filterPlayersByFaction = (players, faction) => {
+        return players.filter(player => player.faction === faction)
+    }
+
     const checkWinCondition = (gameState) => {
-        const village = (
-            gameState.players.filter(player => {
-                player.faction === "Village"
-            }).length
-        );
-        const mafia = (
-            gameState.players.filter(player => {
-                player.faction === "Mafia"
-            }).length
-        );
+        const village = filterPlayersByFaction(gameState.players, "Village").length;
+        const mafia = filterPlayersByFaction(gameState.players, "Mafia").length;
         if (village <= mafia) return {...gameState, winner: "Mafia"};
         if (mafia === 0) return {...gameState, winner: "Village"};
         return null;
-    };
-    if (checkWinCondition(gameState)) return true;
-    console.log(gameState.winner);
+    }
+
+    if (checkWinCondition(gameState) !== null) return true;
     return false;
-    
 }
--- backend/src/isGameOver.test.js
+++ backend/src/isGameOver.test.js
@@ -2,80 +2,6 @@ import { isGameOver } from './isGameOver';
 
 describe("isGameOver", () => {
     it("returns false if the game has not yet ended", () => {
-        const players = [
-            {
-                id: 1,
-                username: "Mary Beth",
-                faction: "Village",
-                alive: true,
-                role: "Villager",
-                vote: null,
-                action: null
-            },
-            {
-                id: 2,
-                username: "Alexander",
-                faction: "Village",
-                alive: true,
-                role: "Villager",
-                vote: null,
-                action: null
-            },
-            {
-                id: 3,
-                username: "Jo",
-                faction: "Village",
-                alive: true,
-                role: "Doctor",
-                vote: null,
-                action: null
-            },
-            {
-                id: 4,
-                username: "Mell",
-                faction: "Village",
-                alive: true,
-                role: "Cop",
-                vote: null,
-                action: null
-            },
-            {
-                id: 5,
-                username: "Ahmed",
-                faction: "Village",
-                alive: true,
-                role: "Villager",
-                vote: null,
-                action: null
-            },
-            {
-                id: 6,
-                username: "Damon",
-                faction: "Mafia",
-                alive: true,
-                role: "Mafioso",
-                vote: null,
-                action: null
-            },
-            {
-                id: 7,
-                username: "Kelsey",
-                faction: "Mafia",
-                alive: true,
-                role: "Mafioso",
-                vote: null,
-                action: null
-            },
-            {
-                id: 8,
-                username: "Steven",
-                faction: "Village",
-                alive: true,
-                role: "Villager",
-                vote: null,
-                action: null
-            },
-        ];
         const gameState = {
             winner: null,
             errorMessage: '',
@@ -84,7 +10,80 @@ describe("isGameOver", () => {
             announcements: '', 
             votes: null, 
             actions: null,
-            players: players
+            players: [
+                {
+                    id: 1,
+                    username: "Mary Beth",
+                    faction: "Village",
+                    alive: true,
+                    role: "Villager",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 2,
+                    username: "Alexander",
+                    faction: "Village",
+                    alive: true,
+                    role: "Villager",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 3,
+                    username: "Jo",
+                    faction: "Village",
+                    alive: true,
+                    role: "Doctor",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 4,
+                    username: "Mell",
+                    faction: "Village",
+                    alive: true,
+                    role: "Cop",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 5,
+                    username: "Ahmed",
+                    faction: "Village",
+                    alive: true,
+                    role: "Villager",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 6,
+                    username: "Damon",
+                    faction: "Mafia",
+                    alive: true,
+                    role: "Mafioso",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 7,
+                    username: "Kelsey",
+                    faction: "Mafia",
+                    alive: true,
+                    role: "Mafioso",
+                    vote: null,
+                    action: null
+                },
+                {
+                    id: 8,
+                    username: "Steven",
+                    faction: "Village",
+                    alive: true,
+                    role: "Villager",
+                    vote: null,
+                    action: null
+                },
+            ]
         };
         expect(isGameOver(gameState)).toBe(false);
     })
