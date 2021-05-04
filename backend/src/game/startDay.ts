185dc20ef080c302574c23c6fd5d9872e6663d52
Steven Yi <steven.yi@berkeley.edu>
2021-05-03 22:26:10 -0600

    changed a bunch of shit

--- backend/src/game/startDay.ts
+++ backend/src/game/startDay.ts
@@ -1,5 +1,6 @@
 import { GamePhase, GameState } from './types'
 
 export const startDay = (gameState: GameState): GameState => {
+    // do I need to push announcements to clients here?
     return { ...gameState, gamePhase: GamePhase.Day }
 }
